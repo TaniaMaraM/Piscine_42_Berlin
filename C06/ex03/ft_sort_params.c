588f12dd6705c3b8ea75254bc03e615916fd69ab
Tania Mara <taniamaramarcosadv@gmail.com>
2024-12-12 16:37:13 +0100

    done

--- C06/ex03/ft_sort_params.c
+++ C06/ex03/ft_sort_params.c
@@ -1,24 +1,73 @@
-/* ************************************************************************** */
+    /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   ft_sort_params.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tmarcos <tmarcos@student.42.fr>            +#+  +:+       +#+        */
+/*   By: tmarcos <tmarcos@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/09/17 15:08:08 by tmarcos           #+#    #+#             */
-/*   Updated: 2024/09/17 15:16:21 by tmarcos          ###   ########.fr       */
+/*   Created: 2024/09/17 19:39:27 by tmarcos           #+#    #+#             */
+/*   Updated: 2024/09/17 22:40:23 by tmarcos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void	ft_putchar(char c)
+int ft_strcmp(char *s1, char *s2)
 {
-	write(1, &c, 1);
+	int i;
+
+	i = 0;
+	while (s1[i] && s2[i] && s1[i] == s2[i])
+		i++;
+	return (s1[i] - s2[i]);
 }
 
-int	swap
-int	main(int argc, char **argv)
+void ft_putstr(char *str)
 {
+	int i;
 
+	i = 0;
+	while (str[i])
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
 }
+
+void ft_swap(char **a, char **b)
+{
+	char *temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+int main(int argc, char **argv)
+{
+	int i;
+	int j;
+
+	i = 1;
+	while (i < argc - 1)
+	{
+		j = 1;
+		while (j < argc - i)
+		{
+			if (ft_strcmp(argv[j], argv[j + 1]) > 0)
+				ft_swap(&argv[j], &argv[j + 1]);
+			j++;
+		}
+		i++;
+	}
+
+	i = 1;
+	while (i < argc)
+	{
+		ft_putstr(argv[i]);
+		write(1, "\n", 1);
+		i++;
+	}
+	return (0);
+}
+
